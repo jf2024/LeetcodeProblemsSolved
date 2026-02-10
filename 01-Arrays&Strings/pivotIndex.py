5dc9c03b7d8fbf2380dda25b4d86a9042305631f
jf2024 <josexfuentes27@gmail.com>
2026-02-09 21:50:58 -0800

    pivotIndex LC problem, good to review

--- 01-Arrays&Strings/pivotIndex.py
+++ 01-Arrays&Strings/pivotIndex.py
@@ -1,6 +1,49 @@
 def pivotIndex(nums: list[int]) -> int:
-    # TODO: Implement logic
-    pass
+    """
+    PREFIX SUM SOLUTION
+
+    TIME: O(N)
+    SPACE: O(N)
+    """
+
+    prefix = [nums[0]]
+    for i in range(1, len(nums)):
+        prefix.append(prefix[-1] + nums[i])
+
+    for i in range(len(nums)):
+        left = prefix[i - 1] if i > 0 else 0
+        right = prefix[-1] - prefix[i] 
+
+        if left == right:
+            return i
+    
+    return -1
+
+def pivotIndex(nums: list[int]) -> int:
+    """
+    VERSION 2
+    WITHOUT PREFIX SUM
+
+    TIME: O(N)
+    SPACE: O(1)
+    """
+    total_sum = sum(nums)
+    left_sum = 0
+        
+    for i in range(len(nums)):
+        # The right sum is everything else MINUS the current number
+        right_sum = total_sum - left_sum - nums[i]
+            
+        if left_sum == right_sum:
+            return i
+            
+        # Prepare the left_sum for the NEXT index
+        left_sum += nums[i]
+            
+    return -1
+
+
+
 
 if __name__ == "__main__":
     # Test 1
