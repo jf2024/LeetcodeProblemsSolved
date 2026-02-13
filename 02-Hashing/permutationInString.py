6f21f4cf46f1edfdc7ea996eadcdebcbdb14d6f8
jf2024 <josexfuentes27@gmail.com>
2026-02-12 22:40:59 -0800

    permutationInString leetcode finished, good problem to review

--- 02-Hashing/permutationInString.py
+++ 02-Hashing/permutationInString.py
@@ -13,45 +13,54 @@ def check_inclusion(s1: str, s2: str) -> bool:
     - 1 <= s1.length, s2.length <= 10^4
     - s1 and s2 consist of lowercase English letters.
 
+    Time Complexity: O(N)
+    Space Complexity: O(1) - since we are just dealing with characters
 
-    "I SOLVED THIS PROBLEM, BUT I HAVE THE FULL SOLUTION ON LEETCODE
 
-    ONE LAST EDGE CASE I DIDNT CONSIDER 
+    Note: I didn't solve this fully on my own, I had to ask gemini for some hints and guidance, specifically:
+        - it mentioned two dictionaries 
+        - as well as fixed size window (sliding window variation)
 
-    ALSO, HAD TO USE GEMINI FOR SOME GUIDANCE AND HELP, IN PARTICULAR:
-        - CLARFYING THE HINT ON LEETCODE THEY GAVE ABOUT FREQUENCIES
-        - GEMINI SUGGESTING COMPARING TWO DICTIONARIES
-        - AND THEN THINKING ABOUT FIXED WINDOW SIZES (SLIDING WINDOW PATTERN)
-
-    ASK IT ABOUT WHAT SHOULD I HAVE BEEN THINKING AOBUT IN THIS PROBLEM? I ORIGINALLY THOUGHT ABOUT SLIDING WINDOW
-    BU NEGLECTED THE FIXED SIZE VARATOIN AND ONLY FOCUSED ON DYNAMIC
-    I ALSO DIDNT CONSIDER USING TWO DICTIONARIES.....
+    And then I solved it on my own 
     """
     s1_dict = Counter(s1)
     s2_dict = defaultdict(int)
 
+    #if s1 is bigger, then return false cause its impossible 
+    if len(s1) > len(s2):
+        return False
+
+    #building our first window 
     for i in range(len(s1)):
         s2_dict[s2[i]] += 1
 
+    #check if that first window is equal, if so return and if not continue to the second loop
     if s1_dict == s2_dict:
         return True
     
 
     for i in range(len(s1), len(s2)):
-        s2_dict[s2[i]] += 1 #add the incoming letter
+        s2_dict[s2[i]] += 1 #add the incoming letter (from the right)
+        s2_dict[s2[i-len(s1)]] -= 1  #delete/remove the "left" letter
 
-        s2_dict[s2[i-len(s1)]] -= 1   #delete/remove the "left" letter
-
-        if s2_dict[s2[i-len(s1)]] == 0:
+        if s2_dict[s2[i-len(s1)]] == 0: 
             del s2_dict[s2[i-len(s1)]]
 
+        #doing our check
         if s1_dict == s2_dict:
             return True
     
-    return False
+    return False   #if we made it this far, couldnt find it so its false
 
 
-
+"""
+Additional Notes to consider: 
+1) How to Recongize this was a fixed sliding window problem:
+    - the problem involves continuous range (subarray string)
+    - the problem gives us a SPECIFIC length
+        - to explain further, a permutation of s1 must be the same length as s1.
+        - ex: if s1 is 3 characters long, we are looking for a "window" that is 3 characters long
+"""
 
 
 def run_tests():
