71a538822d2959526f1aaa46938fbc539d6f8521
jf2024 <josexfuentes27@gmail.com>
2026-02-24 19:41:16 -0800

    two pointers for backspaceCompare problem

--- 04-Stacks&Queues/Stacks/EX_BackspaceCompare.py
+++ 04-Stacks&Queues/Stacks/EX_BackspaceCompare.py
@@ -9,6 +9,8 @@ def backspace_compare(s: str, t: str) -> bool:
     SPACE: O(N + M)
         - store all s characters (O(N))
         - store all t characters (O(M))
+
+    VERSION 1
     """
     stack1 = []
     stack2 = []
@@ -31,6 +33,53 @@ def backspace_compare(s: str, t: str) -> bool:
 
 
     return stack1 == stack2
+
+
+def backspace_compare(s: str, t: str) -> bool:
+    """
+    VERSION 2 USING TWO POINTERS
+    """
+
+    i = len(s) - 1
+    j = len(t) - 1
+    s_skip = 0
+    t_skip = 0
+
+    while i >=0 or j >= 0: #while either string has characters left 
+
+        while i >= 0:   #find valid 's' char
+            if s[i] == '#':
+                s_skip += 1
+                i -= 1
+            elif s_skip > 0:
+                s_skip -= 1
+                i -= 1
+            else:
+                break
+        
+        while j >= 0:   #find valid 'j' char
+            if t[j] == "#":
+                t_skip += 1
+                j -= 1
+            elif t_skip > 0:
+                t_skip -= 1
+                j -= 1
+            else:
+                break
+
+        # if both are valid indices, compare the characters
+        if i >= 0 and j >= 0:
+            if s[i] != t[j]:    #if not equal, then return false even if the indices are the same
+                return False
+            
+        elif (i >= 0) != (j >= 0): #if one is empty and the other isnt, not equal
+            return False
+        
+        i -= 1
+        j -= 1
+
+    return True
+
 
 
 
